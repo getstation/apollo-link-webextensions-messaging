94d7ab75e270754e5f0c993b1c451a83f7c8107a
Joël Charles <joel.charles91@gmail.com>
2020-09-09 18:51:31 +0200

    feat: stringify before send

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import {
   isOperationRequestRPC,
   operationErrorRPC,
   isOperationErrorRPC,
-  isOperationUnsubscribeRPC,
+  isOperationUnsubscribeRPC, parseRPCNotificationMessage,
 } from './rpcs';
 import { MessagingPort, Message } from './types';
 
@@ -53,7 +53,10 @@ export function createWebExtensionMessagingExecutorListener<T extends MessagingP
         };
 
         const operationOnMessageListener = (message: Message): void => {
-          if (isOperationUnsubscribeRPC(message, operationId)) {
+          const m = parseRPCNotificationMessage(message);
+          if (m === null) return;
+
+          if (isOperationUnsubscribeRPC(m, operationId)) {
             close();
           }
         };
@@ -102,14 +105,17 @@ export function createWebExtensionsMessagingLink<T extends MessagingPort>(
       port.postMessage(operationRequestRPC(operationId, operation));
 
       const onMessageListener = (message: Message): void => {
-        if (isOperationResultRPC(message, operationId)) {
-          observer.next(message.params.result);
+        const m = parseRPCNotificationMessage(message);
+        if (m === null) return;
+
+        if (isOperationResultRPC(m, operationId)) {
+          observer.next(m.params.result);
         }
-        if (isOperationCompleteRPC(message, operationId)) {
+        if (isOperationCompleteRPC(m, operationId)) {
           observer.complete();
         }
-        if (isOperationErrorRPC(message, operationId)) {
-          observer.error(new Error(message.params.errorMessage));
+        if (isOperationErrorRPC(m, operationId)) {
+          observer.error(new Error(m.params.errorMessage));
         }
       };
 
--- src/rpcs.ts
+++ src/rpcs.ts
@@ -10,6 +10,22 @@ export type RPCNotificationMessage<TParam> = {
 export const isRPCNotificationMessage = <T>(message: Message): message is RPCNotificationMessage<T> =>
   (message as Record<string, unknown>).jsonrpc === '2.0';
 
+
+
+export const parseRPCNotificationMessage = <T>(message: Message): RPCNotificationMessage<T> | null => {
+  if (!isRPCNotificationMessage(message)) return null;
+  if (typeof message.params !== 'string') return null;
+  try {
+    const parsed = JSON.parse(message.params);
+    return {
+      ...message,
+      params: parsed,
+    }
+  } catch(e) {
+    return null;
+  }
+};
+
 const isRecord = (r: unknown): r is Record<string, unknown> => typeof r === 'object' && r !== null;
 
 // requester -> executor
@@ -21,16 +37,16 @@ export type OperationRequestRPC = RPCNotificationMessage<{
   context: Record<string, unknown>;
 }>;
 export const OPERATION_REQUEST_METHOD = 'operation-request';
-export const operationRequestRPC = (operationId: string, operation: Operation): OperationRequestRPC => ({
+export const operationRequestRPC = (operationId: string, operation: Operation): RPCNotificationMessage<string> => ({
   jsonrpc: '2.0',
   method: OPERATION_REQUEST_METHOD,
-  params: {
+  params: JSON.stringify({
     operationId,
     operationName: operation.operationName,
     variables: operation.variables,
     query: print(operation.query),
     context: operation.getContext(),
-  }
+  })
 });
 export const isOperationRequestRPC = (message: Message): message is OperationRequestRPC =>
   isRPCNotificationMessage(message) && (message.method === OPERATION_REQUEST_METHOD);
@@ -45,13 +61,13 @@ export type OperationResultRPC = RPCNotificationMessage<{
   result: SerializedExecutionResult;
 }>;
 export const OPERATION_RESULT_METHOD = 'operation-result';
-export const operationResultRPC = (operationId: string, result: ExecutionResult): OperationResultRPC => ({
+export const operationResultRPC = (operationId: string, result: ExecutionResult): RPCNotificationMessage<string> => ({
   jsonrpc: '2.0',
   method: OPERATION_RESULT_METHOD,
-  params: {
+  params: JSON.stringify({
     operationId,
     result,
-  }
+  })
 });
 export const isOperationResultRPC = (message: Message, operationId: string): message is OperationResultRPC =>
   isRPCNotificationMessage(message) &&
@@ -64,7 +80,7 @@ export type OperationErrorRPC = RPCNotificationMessage<{
   errorMessage: string;
 }>
 export const OPERATION_ERROR_METHOD = 'operation-error';
-export const operationErrorRPC = (operationId: string, errorValue: unknown): OperationErrorRPC => {
+export const operationErrorRPC = (operationId: string, errorValue: unknown): RPCNotificationMessage<string> => {
   let errorMessage = '<unknow error>';
   if (errorValue instanceof Error) {
     errorMessage = errorValue.message;
@@ -72,10 +88,10 @@ export const operationErrorRPC = (operationId: string, errorValue: unknown): Ope
   return {
     jsonrpc: '2.0',
     method: OPERATION_ERROR_METHOD,
-    params: {
+    params: JSON.stringify({
       operationId,
       errorMessage,
-    }
+    })
   };
 };
 export const isOperationErrorRPC = (message: Message, operationId: string): message is OperationErrorRPC =>
@@ -87,12 +103,12 @@ export const isOperationErrorRPC = (message: Message, operationId: string): mess
 // executor -> requester
 export type OperationCompleteRPC = RPCNotificationMessage<{ operationId: string }>;
 export const OPERATION_COMPLETE_METHOD = 'operation-complete';
-export const operationCompleteRPC = (operationId: string): OperationCompleteRPC => ({
+export const operationCompleteRPC = (operationId: string): RPCNotificationMessage<string> => ({
   jsonrpc: '2.0',
   method: OPERATION_COMPLETE_METHOD,
-  params: {
+  params: JSON.stringify({
     operationId,
-  }
+  })
 });
 export const isOperationCompleteRPC = (message: Message, operationId: string): message is OperationCompleteRPC =>
   isRPCNotificationMessage(message) &&
@@ -104,12 +120,12 @@ export type OperationUnsubscribeRPC = RPCNotificationMessage<{
   operationId: string;
 }>;
 export const OPERATION_UNSUBSCRIBE_METHOD = 'operation-unsubscribe';
-export const operationUnsubscribeRPC = (operationId: string): OperationUnsubscribeRPC => ({
+export const operationUnsubscribeRPC = (operationId: string): RPCNotificationMessage<string> => ({
   jsonrpc: '2.0',
   method: OPERATION_UNSUBSCRIBE_METHOD,
-  params: {
+  params: JSON.stringify({
     operationId,
-  }
+  })
 });
 export const isOperationUnsubscribeRPC = (message: Message, operationId: string): message is OperationUnsubscribeRPC =>
   isRPCNotificationMessage(message) &&
